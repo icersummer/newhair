4a7060bb6051de071d95c2d87312e7326cd7a01a
icersummer <guofangsky@gmail.com>
2015-03-19 09:57:03 +0800

    simple homepage work

--- .apt_generated/com/vj/newhair/activity/MainActivity$$ViewInjector.java
+++ .apt_generated/com/vj/newhair/activity/MainActivity$$ViewInjector.java
@@ -7,9 +7,9 @@ import butterknife.ButterKnife.Finder;
 public class MainActivity$$ViewInjector {
   public static void inject(Finder finder, final com.vj.newhair.activity.MainActivity target, Object source) {
     View view;
-    view = finder.findById(source, 2131296260);
+    view = finder.findById(source, 2131296263);
     if (view == null) {
-      throw new IllegalStateException("Required view with id '2131296260' for field 'mTabHost' was not found. If this view is optional add '@Optional' annotation.");
+      throw new IllegalStateException("Required view with id '2131296263' for field 'mTabHost' was not found. If this view is optional add '@Optional' annotation.");
     }
     target.mTabHost = (android.widget.TabHost) view;
   }
--- src/com/vj/newhair/activity/ZoneActivity.java
+++ src/com/vj/newhair/activity/ZoneActivity.java
@@ -1,10 +1,11 @@
 package com.vj.newhair.activity;
 
-import butterknife.ButterKnife;
-import android.R;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
+import butterknife.ButterKnife;
+
+import com.vj.newhair.R;
 
 public class ZoneActivity extends FragmentActivity{
 	
